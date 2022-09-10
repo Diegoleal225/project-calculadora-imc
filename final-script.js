ae531da006183704370f1eb3e7d411293c9faeb4
Diego Leal <110353826+Diegoleal225@users.noreply.github.com>
2022-09-10 15:14:38 -0300

    Update final-script.js

--- final-script.js
+++ final-script.js
@@ -2,49 +2,78 @@ function escopo(){
     const form=document.querySelector('.form');
     function recebeEvento( evento){
         evento.preventDefault();
-        const firstName= form.querySelector('.name-input').value;
-        const lastName= form.querySelector('.last-name-input').value;
-        const peso= Number(form.querySelector('.peso-input').value);
-        const altura= Number(form.querySelector('.altura-input').value);
-        const idade=Number(form.querySelector('.idade-input').value);
-
-        const imc=(peso/(altura*altura)).toPrecision(4);
-        const spanImcResult= form.querySelector('.p-result');
-        const spanNameResultV=form.querySelector('.name-result');
-        const spanLastName= form.querySelector('.last-name-result');
-        const spanResultComp= form.querySelector('.result-comp');
-        spanNameResultV.innerHTML=firstName;
-        spanLastName.innerHTML=lastName;
-        spanImcResult.innerHTML=imc;
-        spanResultComp.innerHTML=imcResult(imc);
-    }
+        verify();  
+    }    
     form.addEventListener('submit',recebeEvento);
 }
+function verify(){
+    const firstName= document.querySelector('.name-input').value;
+    const lastName= document.querySelector('.last-name-input').value;
+    const peso= Number(document.querySelector('.peso-input').value);
+    const altura= Number(document.querySelector('.altura-input').value);
+    const idade=Number(document.querySelector('.idade-input').value);
+    
+    const imc=(peso/(altura*altura)).toPrecision(4);
+    const spanImcResult= document.querySelector('.p-result');
+    const spanNameResultV=document.querySelector('.name-result');
+    const spanLastName= document.querySelector('.last-name-result');
+    const spanResultComp= document.querySelector('.result-comp');
+
+    if (!firstName){
+        const gridResultContBack= document.querySelector('.grid-result-container').style.background='red';
+        spanNameResultV.innerHTML=` `;
+        spanLastName.innerHTML=` `;
+        spanResultComp.innerHTML=` `;
+        spanImcResult.innerHTML= `Nome invalido `;
+        return ;
+
+    } else if (!lastName){
+        const gridResultContBack= document.querySelector('.grid-result-container').style.background='red';
+        spanNameResultV.innerHTML=` `;
+        spanLastName.innerHTML=` `;
+        spanResultComp.innerHTML=` `;
+        spanImcResult.innerHTML= `Sobre nome invalido `;
+        return ;
+    } else if(!peso){
+        const gridResultContBack= document.querySelector('.grid-result-container').style.background='red';
+        spanNameResultV.innerHTML=` `;
+        spanLastName.innerHTML=` `;
+        spanResultComp.innerHTML=` `;
+        spanImcResult.innerHTML= `Peso invalido ${peso}`;
+        return;
+    }else if (!altura){
+        const gridResultContBack= document.querySelector('.grid-result-container').style.background='red';
+        spanNameResultV.innerHTML=` `;
+        spanLastName.innerHTML=` `;
+        spanResultComp.innerHTML=` `;
+        spanImcResult.innerHTML= `Altura invalida ${altura}`;
+        return;
+
+    }else{
+        spanImcResult.innerHTML=` `;
+        spanNameResultV.innerHTML=firstName;
+        spanLastName.innerHTML=lastName;
+        spanResultComp.innerHTML=imcResult(imc);
+    }
+}
+
 function imcResult(imc){
     const gridResultContBack= document.querySelector('.grid-result-container').style.background='red';
     const gridResultContFont= document.querySelector('.grid-result-container').style.color='white';
     if (imc>=50){
-        return imc='Obesidade gravissima';
+        return imc=` Obesidade gravissima Imc:${imc}`;
     }else if(imc>=40){
-        return imc='Obesidade grave';
+        return imc=` Obesidade grave Imc:${imc}`;
     }else if(imc>=35){
-        return imc='Obesidade moderada';
+        return imc=` Obesidade moderada Imc:${imc}`;
     }else if(imc>25){
-        return imc='Sobrepeso';
+        const gridResultContBack1= document.querySelector('.grid-result-container').style.background=' rgba(255, 255, 0, 0.685)';
+        return imc=` Sobrepeso Imc:${imc}`;
     }else if(imc>=18){
         const gridResultContBack= document.querySelector('.grid-result-container').style.background='#00fa9a';
-        const gridResultContFont= document.querySelector('.grid-result-container').style.color='white';
-        return imc='Peso ideal';
+        return imc=` Peso ideal Imc:${imc}`;
     }else{
         return imc='Abaixo do peso'
     }
 }
 escopo();
-    /*
-    imc=peso/(altura*altura);
-    form.onsubmit=function(evento){
-        evento.preventDefault();
-        alert('Formulario enviado');
-        console.log('Formulario enviado')
-    };
- */
